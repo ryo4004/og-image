030ad00efd619e2b6fabb3fd3ac7778a0e528ef0
ryo4004 <kato@ryo4004.net>
2021-08-19 23:46:23 +0900

    絵文字のみ表示する

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -1,46 +1,17 @@
-import marked from 'marked';
-import { sanitizeHtml } from './sanitizer';
 import { ParsedRequest } from './types';
 const twemoji = require('twemoji');
 const twOptions = { folder: 'svg', ext: '.svg' };
 const emojify = (text: string) => twemoji.parse(text, twOptions);
 
-function getCss(theme: string, fontSize: string) {
-    let background = 'white';
-    let foreground = 'black';
-
-    if (theme === 'dark') {
-        background = 'black';
-        foreground = 'white';
-    }
+function getCss() {
     return `
-    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
-
-    @font-face {
-        font-family: 'noto-sans-cjk-jp';
-        font-style: normal;
-        font-weight: normal;
-    }
-
     body {
-        background: ${background};
         background-image: linear-gradient(160deg, #b7f8db 0%, #50a7c2 100%);
         height: 100vh;
         display: flex;
         text-align: center;
         align-items: center;
         justify-content: center;
-    }
-
-    code {
-        color: #D400FF;
-        font-family: 'Vera';
-        white-space: pre-wrap;
-        letter-spacing: -5px;
-    }
-
-    code:before, code:after {
-        content: '\`';
     }
 
     .logo-wrapper {
@@ -54,59 +25,23 @@ function getCss(theme: string, fontSize: string) {
     .logo-wrapper .emoji {
         width: 768px;
         height: 768px;
-    }
-
-    .logo {
-        margin: 0 75px;
-    }
-
-    .plus {
-        color: #BBB;
-        font-family: Times New Roman, Verdana;
-        font-size: 100px;
-    }
-
-    .spacer {
-        margin: 150px;
-    }
-
-    .emoji {
-        height: 1em;
-        width: 1em;
-        margin: 0 .05em 0 .1em;
-        vertical-align: -0.1em;
-    }
-    
-    .heading {
-        display: none;
-        font-family: 'noto-sans-cjk-jp', 'Inter', sans-serif;
-        font-size: ${sanitizeHtml(fontSize)};
-        font-style: normal;
-        color: ${foreground};
-        line-height: 1.8;
     }`;
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
-    const { text, theme, md, fontSize, emoji } = parsedReq;
+    const { emoji } = parsedReq;
     return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
     <title>Generated Image</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
-        ${getCss(theme, fontSize)}
+        ${getCss()}
     </style>
     <body>
         <div>
-            <div class="spacer">
             <div class="logo-wrapper">
                 ${emojify(emoji)}
-            </div>
-            <div class="spacer">
-            <div class="heading">${emojify(
-                md ? marked(text) : sanitizeHtml(text)
-            )}
             </div>
         </div>
     </body>
